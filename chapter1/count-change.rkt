a032d380319a3d46db603f31d5a41968a4c58597
Quang Minh Tran <minhquang104@gmx.de>
2017-02-11 21:50:14 +0100

    Fix solution for count change

--- chapter1/count-change.rkt
+++ chapter1/count-change.rkt
@@ -1,4 +1,7 @@
- amount kinds-of-coins)
+#lang racket
+
+; Recurisvely calculate the number of possible changes
+(define (cc amount kinds-of-coins)
   (cond ((= amount 0) 1)
         ((or (< amount 0) (= kinds-of-coins 0)) 0)
         (else (+ (cc amount
@@ -7,6 +10,7 @@
                         (first-denomination kinds-of-coins))
                      kinds-of-coins)))))
 
+; Return the denomination of the first coin
 (define (first-denomination kinds-of-coins)
   (cond ((= kinds-of-coins 1) 1)
         ((= kinds-of-coins 2) 5)
@@ -14,10 +18,11 @@
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
 
+; Return the number of possible ways of making change of amount
 (define (count-change amount)
   (cc amount 5))
-;(count-change 100)
 
+; Recursively calculate all possible change solutions
 (define (dc amount kinds-of-coins coins)
   (cond ((= amount 0) (list coins))
         ((or (< amount 0) (= kinds-of-coins 0)) null)
@@ -29,8 +34,9 @@
                           kinds-of-coins
                           (cons (first-denomination kinds-of-coins) coins))))))
 
+; Display all possible ways of making change of amount
 (define (display-change amount)
   (dc amount 5 null))
 
+(count-change 20)
 (display-change 20)
-
