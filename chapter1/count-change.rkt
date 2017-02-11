5e3237dedecc04184ff3ea1358257fbc772d3571
Quang Minh Tran <minhquang104@gmx.de>
2017-02-11 22:05:24 +0100

    Make kinds of coins configurable

--- chapter1/count-change.rkt
+++ chapter1/count-change.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-; Recurisvely calculate the number of possible changes
+; Recurisvely calculate the number of possible changes. OMG the sicp's solution is soooo beautifully!
 (define (cc amount kinds-of-coins)
   (cond ((= amount 0) 1)
         ((or (< amount 0) (= kinds-of-coins 0)) 0)
@@ -38,5 +38,23 @@
 (define (display-change amount)
   (dc amount 5 null))
 
+; Recursively calculate all possible change solutions. The kinds-of-coins are passed as argument.
+(define (dc-configurable-coins amount kinds-of-coins coins)
+  (cond ((= amount 0) (list coins))
+        ((or (< amount 0) (null? kinds-of-coins)) null)
+        (else (append (dc-configurable-coins amount
+                          (cdr kinds-of-coins)
+                          coins)
+                      (dc-configurable-coins (- amount
+                             (car kinds-of-coins))
+                          kinds-of-coins
+                          (cons (car kinds-of-coins) coins))))))
+
+; Display all possible ways of making change solutions. The kinds of coins are configurable.
+(define (display-change-configurable-coins amount kinds-of-coins)
+  (dc-configurable-coins amount kinds-of-coins null))
+
+
 (count-change 20)
 (display-change 20)
+(display-change-configurable-coins 20 (list 50 25 10 5 1))
